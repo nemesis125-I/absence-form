cb4f66c55f0c5484b9442cc440b8282f4b917d85
nemesis125-I <nemesis125@sungseo.ms.kr>
2026-09-03 13:03:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 선생님이 GitHub Pages에 올린 뒤 딱 한 곳만 수정합니다.
 const CONFIG = {
-  GAS_URL: "여기에_배포된_Google_Apps_Script_Web_앱_URL을_붙여넣으세요",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxfZrRqepameVoQKn2pnfXuSzNS523qGUoPKy58NgofXU4wed9CYUKnXaDhl87Jqi7I/exec",
   TEACHER_PASSWORD: "4341",
   SCHOOL_NAME: "성서중학교"
 };
